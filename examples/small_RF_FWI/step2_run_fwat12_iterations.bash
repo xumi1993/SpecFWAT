4b1cfd67a264e9465d841b74e3b2135f5e60bb2f
xumi1993 <gomijianxu@gmail.com>
2024-07-20 17:30:28 -0400

    update example

--- examples/small_RF_FWI/step2_run_fwat12_iterations.bash
+++ examples/small_RF_FWI/step2_run_fwat12_iterations.bash
@@ -10,6 +10,8 @@ for name in vs vp rho; do
   mpirun -np $NPROC ../../bin/xdecompose_h5_gll ${name} ./initial_model.h5 model_initial/
 done
 
+cp model_initial/* DATABASES_MPI/
+
 # Run the fullwaveform adjoint tomography with 2 iterations
 mpirun -np $NPROC ../../bin/xfullwave_adjoint_tomo 0 2
 
