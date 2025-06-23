7baaee9f52162c23f2a4c44e51bfc4684ed4c2af
xumi1993 <gomijianxu@gmail.com>
2025-06-24 07:48:35 +0800

    add "free shm"

--- src/preproc/fwat_fwd_measure_adj.f90
+++ src/preproc/fwat_fwd_measure_adj.f90
@@ -51,6 +51,9 @@ do while (ffwd%ievt <= nsim)
   ffwd%ievt = ffwd%ievt + 1
 enddo
 
+! Free shared memory
+call fpar%acqui%finalize()
+
 call log%write('*******************************************', .false.)
 call log%write('*********** PRE-PROCESSING DONE ***********', .false.)
 call log%write('*******************************************', .false.)
