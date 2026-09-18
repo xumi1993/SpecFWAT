e8d8a3abb176744378b7f4c19dcddf468b63e495
xumi1993 <gomijianxu@gmail.com>
2026-09-17 21:10:33 -0400

    Rename fwat_post_proc to fwat_rtm and update CMakeLists.txt to include the new source file

--- src/postproc/fwat_rtm.f90
+++ src/postproc/fwat_rtm.f90
@@ -1,8 +1,9 @@
-program fwat_post_proc
+program fwat_rtm
   use fwat_mpi
   use config
   use post_processing, only: remove_ekernel
-  uer post_rtm, only: PostRTM
+  use post_rtm, only: PostRTM
+  use logger, only: log
   use input_params, fpar => fwat_par_global
   use argparse, only: parse_args_post_process
   use param_check, only: check_model_name, check_postproc_params, check_nevents
@@ -22,30 +23,22 @@ program fwat_post_proc
   call fpp%init()
 
   call fpp%sum_kernel()
-      
+
   call fpp%apply_precond()
-    
+
   call remove_ekernel()
 
   call fpar%acqui%finalize()
 
   call fpp%finalize()
 
-      call synchronize_all()
-    end if
-  end do
-
-  if (is_joint) then
-    call sum_joint_kernel_grid()
-  endif
-
   call log%write('*******************************************', .false.)
   call log%write('********** POST-PROCESSING DONE ***********', .false.)
   call log%write('*******************************************', .false.)
   call log%finalize()
-  
+
   call synchronize_all()
 
   call finalize_mpi()
-    
-end program fwat_post_proc
+
+end program fwat_rtm
